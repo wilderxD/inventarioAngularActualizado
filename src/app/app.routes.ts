1cfa06386844cd19e296cb3837cb1c13b8729a3e
wilderxD <wilder1093@gmail.com>
2025-12-03 17:41:27 -0500

    new

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,10 +7,12 @@ import { RecursosComponent } from './components/recursos-component/recursos-comp
 import { CargamasivaComponent } from './components/cargamasiva-component/cargamasiva-component';
 import { DbComponent } from './components/db-component/db-component';
 import { AsignadosComponent } from './components/asignados-component/asignados-component';
+import { FormularioEquipo } from './components/equipo-component/formulario-equipo/formulario-equipo';
 
 export const routes: Routes = [
    { path: 'inicio', component: InicioComponents},
    { path: 'listarEquipo', component: EquipoComponent},
+   { path: 'listarEquipo/form', component: FormularioEquipo},
    { path: 'listarUsuarios', component: AdministracionComponent },
    { path: 'listarInventario', component: InventarioComponent },
    { path: 'recursos', component: RecursosComponent },
---
+++ src/app/components/equipo-component/formulario-equipo/formulario-equipo.ts
@@ -0,0 +1,99 @@
+import { Component, OnInit } from '@angular/core';
+import { Equipo } from '../../../models/equipo';
+import { Moneda } from '../../../models/moneda';
+import { Categoria } from '../../../models/categoria';
+import { Estado } from '../../../models/estado';
+import { EquipoService } from '../../../services/equipo-service';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import Swal from 'sweetalert2';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-formulario-equipo',
+  standalone: true,
+  imports: [RouterModule, CommonModule, FormsModule],
+  templateUrl: './formulario-equipo.html',
+  styleUrl: './formulario-equipo.css',
+})
+export class FormularioEquipo implements OnInit{
+
+  public equipo : Equipo = new Equipo();
+  public monedas: Moneda[] = [];
+  public categorias: Categoria[] = [];
+  public estados: Estado[] = [];
+  public titulo: string = "Crear Equipo";
+
+  public errores: string[] = [];
+
+  constructor(
+    private _equipoService: EquipoService,
+    private _router: Router,
+    private _activatedRoute: ActivatedRoute
+  ){}
+
+  ngOnInit(): void {
+    this.cargarEquipo();
+  }
+
+  cargarEquipo(): void {
+    this._activatedRoute.params.subscribe(params => {
+      let id = params['id'];
+      if(id){
+        this._equipoService.getEquipo(id).subscribe((equipo) => this.equipo = equipo);
+      }
+    });
+    this._equipoService.getMenedas().subscribe(monedas => this.monedas = monedas);
+    this._equipoService.getCategorias().subscribe(categorias => this.categorias = categorias);
+    this._equipoService.getEstados().subscribe(estados => this.estados = estados);
+  }
+
+  public create(): void{
+    this._equipoService.create(this.equipo).subscribe(
+      equipo => {
+        this._router.navigate(['/listarEquipo']),
+        Swal.fire('Nuevo Equipo', `El equipo ${equipo.descripcion} ha sido creado con éxito`, 'success');
+      },
+      err => {
+        this.errores = err.error.errors as string[];
+        console.error("Codigo del error desde el backend: " + err.status);
+        console.error(err.error.errors);
+      }
+    );
+  }
+
+  public update(): void{
+    this._equipoService.update(this.equipo).subscribe(json => {
+      this._router.navigate(['/listarEquipo']),
+      Swal.fire('Equipo Actualizado', `El equipo ${json.equipo.descripcion}`, 'success');
+    },
+    err => {
+      this.errores = err.error.errors as string[];
+      console.error("Codigo del error desde el backend: " + err.status);
+      console.error(err.error.errors);
+    }
+  );
+  }
+
+  public compararMoneda(m1: Moneda, m2: Moneda): boolean {
+    if(m1 == undefined && m2 == undefined){
+      return true;
+    }
+    return m1 == null || m2 == null || m1 == undefined || m2 == undefined ? false : m1.id == m2.id
+  }
+
+  public compararEstado(e1: Estado, e2: Estado): boolean {
+    if(e1 == undefined && e2 == undefined){
+      return true;
+    }
+    return e1 == null || e2 == null || e1 == undefined || e2 == undefined ? false : e1.id == e2.id
+  }
+
+  public compararCategoria(c1: Categoria, c2: Categoria): boolean{
+    if(c1 == undefined && c2 == undefined){
+      return true;
+    }
+    return c1 == null || c2 == null || c1 == undefined || c2 == undefined ? false : c1.id == c2.id
+  }
+
+}
